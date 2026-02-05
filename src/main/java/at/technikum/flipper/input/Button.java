11f8bff1b4a0ddf32fcbadfa79523e716af4bb73
ww24e018 <ww24e018@technikum-wien.at>
2026-02-05 17:28:05 +0100

    transfer commit (changed button)

--- src/main/java/at/technikum/flipper/input/Button.java
+++ src/main/java/at/technikum/flipper/input/Button.java
@@ -1,5 +1,7 @@
 package at.technikum.flipper.input;
 
+import java.lang.reflect.Method;
+
 public class Button {
 
     protected final InputMediator mediator;
@@ -8,7 +10,19 @@ public class Button {
         this.mediator = inputMediator;
     }
 
-    public void wasJustPressed() {
+    public void wasJustPressed()  {
+        var mediatorClazz = this.mediator.getClass();
+        var selfClazz = this.getClass();
+        Method handler;
+        System.out.format("<Button>: mediatorClazz.toString = %s\n", mediatorClazz.toString());
+        try {
+            handler = mediatorClazz.getDeclaredMethod("notifyOfPress", selfClazz);
+        } catch (NoSuchMethodException e) {
+            handler = mediatorClazz.getDeclaredMethod("notifyOfPress", Button.class);
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        }
+        handler.invoke(this);
         this.mediator.notifyOfPress(this);
     }
     public void wasJustReleased() {
