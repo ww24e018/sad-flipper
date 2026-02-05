953746adedc1971f1dfcf1054181f642482a94d0
ww24e018 <ww24e018@technikum-wien.at>
2026-02-05 13:11:07 +0100

    replace all

---
+++ README.md
@@ -0,0 +1,3 @@
+reboot of sad-flipper with pre-scaffold version of slm-t3
+
+hungry for unittests
--- flipper/src/main/java/at/technikum/flipper/command/Command.java
+++
@@ -1,5 +0,0 @@
-package at.technikum.flipper.command;
-
-public interface Command {
-    public void execute();
-}
